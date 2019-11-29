987f610f1cb1a032dac8350481e548928c591079
Danielrcnn <56006459+Danielrcnn@users.noreply.github.com>
2019-11-29 11:02:57 -0300

    Add files via upload

--- Questao4.m
+++ Questao4.m
@@ -4,26 +4,26 @@ clc;
 disp('Programa MLP para XOR 2 entradas');
 X = [ -1 , 1 , -1 , 1 ;-1 , -1 , 1 , 1 ];
 Yd =[ -1 , 1 , 1 , -1 ];
-neuronios_camada_escondida = 2;
+neuronios_camada_escondida = 2; 
 disp('Criando a rede MLP...');
-net = feedforwardnet(neuronios_camada_escondida);
+net = feedforwardnet(neuronios_camada_escondida); %Camadas ocultas
 disp('Configurando a rede...');
-net = configure(net,X,Yd);
-net.divideParam.trainRatio = 1;
-net.divideParam.valRatio = 0;
+net = configure(net,X,Yd); %configura entradas e saídas da rede
+net.divideParam.trainRatio = 1; %Amostras para treinamento
+net.divideParam.valRatio = 0;%Amostras para classificação
 net.divideParam.testRatio = 0;
-net.trainFcn = 'traingd';
-net.performFcn = 'mse';
-net.trainParam.epochs = 500000;
-net.trainParam.lr = 0.1;
-net.trainParam.mc = 0;
-net.trainParam.goal = 0.0001;
-net.layers{1}.transferFcn = 'tansig';
-net.layers{2}.transferFcn = 'tansig';
+net.trainFcn = 'traingd'; %Tipo da função de ativação
+net.performFcn = 'mse';%Tipo de erro a ser calculado
+net.trainParam.epochs = 500000; %Maximo de epocas
+net.trainParam.lr = 0.1;%Taxa de Aprendizagem
+net.trainParam.mc = 0; %Momento 
+tansig.goal = 0.0001;
+net.layers{1}.transferFcn = 'tansig';%Tipo da função de ativação em cada camada
+net.layers{2}.transferFcn = 'tansig';%Tipo da função de ativação em cada camada
 disp('Inicializando a rede neural....');
 net = init(net);
 disp('Treinando a rede neural...');
-[net, tr] = train(net,X,Yd);
+[net, tr] = train(net,X,Yd); %Treina a rede neural
 plotperform(tr);
 disp('Simulando a rede neural treinada...');
 Ysaida = sim(net,X);
